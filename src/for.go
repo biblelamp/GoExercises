3f842b04fa44389db2be3560c1d54020cee3c463
Sergey Iryupin <biblelamp@yandex.ru>
2019-03-25 08:34:47 +0100

    for example

---
+++ src/for.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    i := 1
+    for i <= 5 {
+        fmt.Print(i, " ")
+        i = i + 1
+    }
+    fmt.Println()
+    for i := 1; i <= 10; i++ {
+        fmt.Print(i, " ")
+    }
+    fmt.Println()
+    str := "Test String"
+    for i, ch := range str {
+        fmt.Println("Code", ch, "of char in string No", i)
+    }
+}
