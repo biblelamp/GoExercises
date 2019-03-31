a5560d4635114bc15c488909d9f87fee33774edd
Sergey Iryupin <biblelamp@yandex.ru>
2019-03-31 20:05:20 +0200

    slice example

---
+++ src/slice.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  scores := make([]int, 0, 5)
+  c := cap(scores)
+  fmt.Println(c)
+
+  for i := 0; i < 25; i++ {
+    scores = append(scores, i)
+
+    // if capacity is changed, 
+    // Go increases the array to adapt to new data
+    if cap(scores) != c {
+      c = cap(scores)
+      fmt.Println(c)
+    }
+  }
+}
