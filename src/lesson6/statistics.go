f515e5f726f1b5b80e0a5bd71b6a993441ed5bad
Sergey Iryupin <biblelamp@yandex.ru>
2019-04-14 18:51:20 +0200

    preparing for testing

---
+++ src/lesson6/statistics.go
@@ -0,0 +1,13 @@
+package statistics
+
+// save to %GOPATH%/src/statistics
+// move to this folder and make:
+// > go install
+
+func Average(xs []float64) float64 {
+    total := float64(0)
+    for _, x := range xs {
+        total += x
+    }
+    return total / float64(len(xs))
+}
