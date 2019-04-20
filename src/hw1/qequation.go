adf1a6ff9b718184d835c93af9b114445a6a0bd2
Sergey Iryupin <biblelamp@yandex.ru>
2019-04-20 15:10:11 +0200

    changed name of functions

--- src/hw1/qequation.go
+++ src/hw1/qequation.go
@@ -1,28 +1,31 @@
 package main
 
-import ("fmt"; "math")
+import (
+	"fmt"
+	"math"
+)
 
 // solution quadratic equation
 
-var a, b, c float64
+func qequation() {
 
-func main() {
+	var a, b, c float64
 
-    fmt.Println("Enter a, b, c:")
-    fmt.Scanln(&a, &b, &c)
+	fmt.Println("Enter a, b, c:")
+	fmt.Scanln(&a, &b, &c)
 
-    d := b*b - 4*a*c;
+	d := b*b - 4*a*c
 
-    //fmt.Println(d)
+	//fmt.Println(d)
 
-    if d < 0 {
-        fmt.Println("No root of equation")
-    } else if d == 0 {
-        x := -b/(2*a)
-        fmt.Println("x =", x)
-    } else {
-        x1 := (-b + math.Sqrt(d))/(2*a)
-        x2 := (-b - math.Sqrt(d))/(2*a)
-        fmt.Println("x1 =", x1, "x2 =", x2)
-    }
+	if d < 0 {
+		fmt.Println("No root of equation")
+	} else if d == 0 {
+		x := -b / (2 * a)
+		fmt.Println("x =", x)
+	} else {
+		x1 := (-b + math.Sqrt(d)) / (2 * a)
+		x2 := (-b - math.Sqrt(d)) / (2 * a)
+		fmt.Println("x1 =", x1, "x2 =", x2)
+	}
 }
--- src/hw1/rtriangle.go
+++ src/hw1/rtriangle.go
@@ -1,19 +1,22 @@
 package main
 
-import ("fmt"; "math")
+import (
+	"fmt"
+	"math"
+)
 
 // calculation of a right triangle
 
-var a, b float64
+func triangle() {
 
-func main() {
+	var a, b float64
 
-    fmt.Println("Enter legs of right triangle (a, b):")
-    fmt.Scanln(&a, &b)
+	fmt.Println("Enter legs of right triangle (a, b):")
+	fmt.Scanln(&a, &b)
 
-    s := a*b / 2;
-    g := math.Sqrt(a*a + b*b)
-    p := a + b + g
-    fmt.Println("s =", s, "g =", g, "p =", p)
+	s := a * b / 2
+	g := math.Sqrt(a*a + b*b)
+	p := a + b + g
+	fmt.Println("s =", s, "g =", g, "p =", p)
 
 }
