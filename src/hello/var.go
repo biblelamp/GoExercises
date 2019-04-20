aa59dd7dbe20e0e5b202c27ec7fdf55be9e78001
Sergey Iryupin <biblelamp@yandex.ru>
2019-04-20 08:51:50 +0200

    init hello folder

---
+++ src/hello/var.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func variables() {
+
+	a := new(int)
+	b := new(int)
+	*a = 1
+	*b = 2
+	fmt.Printf("The value of the variable a: %x, b: %x \n", *a, *b)
+	a = b
+	fmt.Printf("The value of the variable a: %x, b: %x \n", *a, *b)
+	*a = *a + 1
+	*b = *b + 1
+	fmt.Printf("The value of the variable a: %x, b: %x \n", *a, *b)
+
+}
