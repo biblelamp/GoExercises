8e6499da5946c75dc456cbef87f2d75011c72d73
Sergey Iryupin <biblelamp@yandex.ru>
2019-04-22 19:21:49 +0200

    fixed a bit

--- src/hw1/hw1.go
+++ src/hw1/hw1.go
@@ -2,7 +2,7 @@ package main
 
 /*
  * Syntax Go. Homework 1
- * Sergey Iryupin, dated Apr 21, 2019
+ * Sergey Iryupin, dated Apr 22, 2019
  */
 
 import (
@@ -23,7 +23,7 @@ func main() {
 	fmt.Printf("%.2f RUB = %.2f USD\n", amountRub, convertRUBToUSD(amountRub, rate))
 
 	fmt.Println("2. Calculation of a right triangle. Enter triangle legs:")
-	fmt.Scanln(&legOne, &legTwo)
+	_, err = fmt.Scanln(&legOne, &legTwo)
 	if err != nil {
 		return
 	}
@@ -31,7 +31,7 @@ func main() {
 	fmt.Printf("Hypotenuse = %.2f Area = %.2f Perimeter = %.2f\n", hypotenuse, area, perimeter)
 
 	fmt.Println("3. Calculation of bank deposit. Enter amount and percent:")
-	fmt.Scanln(&amount, &percent)
+	_, err = fmt.Scanln(&amount, &percent)
 	if err != nil {
 		return
 	}
@@ -49,7 +49,7 @@ func convertRUBToUSD(amountRub, rate float64) float64 {
 
 func calcTriangle(legOne, legTwo float64) (hypotenuse, area, perimeter float64) {
 	hypotenuse = math.Sqrt(math.Pow(legOne, 2) + math.Pow(legTwo, 2))
-	area = legOne * legTwo
+	area = legOne * legTwo / 2
 	perimeter = legOne + legTwo + hypotenuse
 	return
 }
