2b7bb2a9aaa33fe98c0ed73f1aad90ce9d160706
Sergey Iryupin <biblelamp@yandex.ru>
2019-04-29 15:30:11 +0200

    improved a bit

--- src/lesson3/slice.go
+++ src/lesson3/slice.go
@@ -4,18 +4,20 @@ import "fmt"
 
 func main() {
 
-  scores := make([]int, 0, 5)
-  c := cap(scores)
-  fmt.Println(c)
+	scores := make([]int, 0, 5)
+	c := cap(scores)
+	fmt.Println(c)
 
-  for i := 0; i < 25; i++ {
-    scores = append(scores, i)
+	for i := 0; i < 25; i++ {
+		scores = append(scores, i)
 
-    // if capacity is changed, 
-    // Go increases the array to adapt to new data
-    if cap(scores) != c {
-      c = cap(scores)
-      fmt.Println(c)
-    }
-  }
+		fmt.Printf(" %d", i+1)
+
+		// if capacity is changed,
+		// Go increases the array to adapt to new data
+		if cap(scores) != c {
+			c = cap(scores)
+			fmt.Printf("\n%d\n", c)
+		}
+	}
 }
