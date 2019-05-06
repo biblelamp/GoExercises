602127527985fe72617cd28b173183c900678276
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-06 16:58:38 +0200

    fixed bug in line #12

--- src/hw4.chess.go
+++ src/hw4.chess.go
@@ -9,7 +9,7 @@ type Point struct {
 
 func (point Point) nextPoint(dx, dy int) (next Point, ok bool) {
 	next = Point{point.x + dx, point.y + dy}
-	ok = next.x >= 0 && next.y >= 0
+	ok = next.x > -1 && next.y > -1 && next.x < 8 && next.y < 8
 	return
 }
 
@@ -30,7 +30,9 @@ func main() {
 	point2x2 := Point{2, 2}
 	point0x2 := Point{0, 2}
 	point0x0 := Point{0, 0}
+	point7x7 := Point{7, 7}
 	fmt.Println(point2x2.nextPoints())
 	fmt.Println(point0x2.nextPoints())
 	fmt.Println(point0x0.nextPoints())
+	fmt.Println(point7x7.nextPoints())
 }
