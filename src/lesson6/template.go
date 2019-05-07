50bd2dbc9a0d70bbdc62cc4473104d6073fbaae2
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-07 08:49:17 +0200

    example of template

---
+++ src/lesson6/template.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("template.html"))
+
+	data := TodoPageData{
+		PageTitle: "TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(os.Stdout, data)
+}
