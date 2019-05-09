118efae2ece72e9dc62cd040e5af437e6d65da28
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-09 19:19:00 +0200

    refactored a bit

--- src/lesson6/http.go
+++ src/lesson6/http.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-    "net/http"
-    "io"
+	"io"
+	"net/http"
 )
 
 func hello(res http.ResponseWriter, req *http.Request) {
-    res.Header().Set(
-        "Content-Type", 
-        "text/html",
-    )
-    io.WriteString(
-        res, 
-        `<doctype html>
+	res.Header().Set("Content-Type", "text/html")
+	io.WriteString(res,
+		`<doctype html>
 <html>
-    <head>
-        <title>Hello World</title>
-    </head>
-    <body>
-        Hello World!
-    </body>
-</html>`,
-    )
+	<head>
+		<title>Hello World!</title>
+	</head>
+	<body>
+		Hello World!
+	</body>
+</html>`)
 }
 
 func main() {
-    http.HandleFunc("/hello", hello)
-    http.ListenAndServe(":8080", nil)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.HandleFunc("/hello", hello)
+
+	http.ListenAndServe(":80", nil)
 }
