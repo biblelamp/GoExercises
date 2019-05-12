46fdcc8d6955a77d5e8a6a1fe2a8a41aad04819a
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-12 17:45:35 +0200

    added a bit

--- src/lesson7/timesrv.go
+++ src/lesson7/timesrv.go
@@ -25,7 +25,7 @@ func main() {
 func handleConn(c net.Conn) {
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n\r"))
 		if err != nil {
 			return
 		}
