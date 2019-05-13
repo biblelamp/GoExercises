7929157deecfacb66b4d524e459e794a12ffcf0e
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-13 17:13:06 +0200

    improved a bit

--- src/lesson6/png.go
+++ src/lesson6/png.go
@@ -12,9 +12,7 @@ import (
 func main() {
 
 	rectangle := "rectangle.png"
-
 	green := color.RGBA{0, 255, 0, 255}
-
 	rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
 
 	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
@@ -23,5 +21,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed create file: %s", err)
 	}
+	defer file.Close()
 	png.Encode(file, rectImg)
 }
