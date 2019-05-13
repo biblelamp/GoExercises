0379e742fed4009c6d7bdf6c2484437211957944
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-13 17:11:00 +0200

    improved a bit

--- src/lesson6/html.go
+++ src/lesson6/html.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	s1 := `"Fran & Freddie's Diner" <tasty@example.com>`
 	s2 := html.EscapeString(s1)
+	s3 := html.UnescapeString(s2)
 	fmt.Println(s2)
-	fmt.Println(html.UnescapeString(s2))
+	fmt.Println(s3)
 }
