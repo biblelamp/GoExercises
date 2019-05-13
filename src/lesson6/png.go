c75d35a23f60a1040bfeebaeee769b0d18943ae4
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-13 20:54:28 +0200

    changed a bit

--- src/lesson6/png.go
+++ src/lesson6/png.go
@@ -11,13 +11,12 @@ import (
 
 func main() {
 
-	rectangle := "rectangle.png"
 	green := color.RGBA{0, 255, 0, 255}
 	rectImg := image.NewRGBA(image.Rect(0, 0, 200, 200))
 
 	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
 
-	file, err := os.Create(rectangle)
+	file, err := os.Create("rectangle.png")
 	if err != nil {
 		log.Fatalf("Failed create file: %s", err)
 	}
