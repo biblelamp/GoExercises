eeb4d324f4831ec44e2db38ad7ede16ec5d27004
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-19 06:34:57 +0200

    example of countdown

---
+++ src/lesson7/countdown.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Countdown start...")
+	tick := make(<-chan time.Time)    // creating unidirectional chanel
+	tick = time.Tick(1 * time.Second) // creating tick flow
+	for countdown := 10; countdown > 0; countdown-- {
+		moment := <-tick
+		fmt.Println(moment.Format("15:04:05"), countdown)
+	}
+	fmt.Println("The rocket starts!")
+}
