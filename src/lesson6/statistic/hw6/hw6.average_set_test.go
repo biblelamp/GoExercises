a288914d970283d25c4334399004743094d75d68
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-20 14:03:56 +0200

    added one more example of nw #6

---
+++ src/lesson6/statistic/hw6/hw6.average_set_test.go
@@ -0,0 +1,41 @@
+package hw6
+
+import "testing"
+
+type testpair struct {
+	values  []float64
+	average float64
+	total   float64
+}
+
+var tests = []testpair{
+	{[]float64{1, 2}, 1.5, 3},
+	{[]float64{1, 1, 1, 1, 1, 1}, 1, 6},
+	{[]float64{-1, 1}, 0, 0},
+}
+
+func TestAverageSet(t *testing.T) {
+	for _, pair := range tests {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
+	}
+}
+
+func TestSumSet(t *testing.T) {
+	for _, pair := range tests {
+		v := Sum(pair.values)
+		if v != pair.total {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.total,
+				"got", v,
+			)
+		}
+	}
+}
