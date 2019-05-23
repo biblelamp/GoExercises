9890c30f15ec022b07bc37323c3a67a686cb1c8a
Sergey Iryupin <biblelamp@yandex.ru>
2019-05-24 00:15:40 +0200

    simplified

--- src/lesson7/nc.go
+++ src/lesson7/nc.go
@@ -8,16 +8,10 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost: 8000")
+	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	dataCopy(os.Stdout, conn)
-}
-
-func dataCopy(dst io.Writer, src io.Reader) {
-	if _, err := io.Copy(dst, src); err != nil {
-		log.Fatal(err)
-	}
+	io.Copy(os.Stdout, conn) // NOTE: ignoring errors
 }
