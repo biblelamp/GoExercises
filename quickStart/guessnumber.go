31427f8cab3864c9acbd78c7b3e83e396c52224e
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-18 15:21:29 +0200

    guess the number

---
+++ quickStart/guessnumber.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+    limit := 3
+    count := 0
+    guess := -1
+    number := rand.Intn(10)
+    for count < limit && guess != number {
+        fmt.Print("Guess [", limit - count, " attempts] the number (0..9):")
+        fmt.Scanln(&guess)
+        if number != guess {
+            if guess > number {
+                fmt.Println("Your number is greater")
+            } else {
+                fmt.Println("Your number is less")
+            }
+        count++
+        }
+    }
+}
