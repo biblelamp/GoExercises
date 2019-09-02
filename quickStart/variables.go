f43e0bcc4f14697d427709cde8ee45de71b22c86
Sergey Iryupin <biblelamp@yandex.ru>
2019-09-02 23:14:59 +0200

    added a bit

--- quickStart/variables.go
+++ quickStart/variables.go
@@ -1,5 +1,13 @@
 package main
 
+    // 11 целочисленных типов: int8, int16, int32, int64, uint8, uint16, uint32, uint64
+    //      int, uint, byte (uint8), rune (uint32), uintptr
+    // 2 типа с плавающей точкой: float32, float64
+    // 3 типа с неограниченной точностью (пакет big): big.Int, big.Rat, big.Float
+    // 2 типа комплексных чисел: complex64, complex128
+    // 1 логический тип: bool
+    // 1 строка: string
+
 func main() {
     i := 4
     f := 4.5
