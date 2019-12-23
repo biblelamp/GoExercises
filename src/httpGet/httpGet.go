ecebe9f871ecdbf92bf5648f821950815dca61a8
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-23 20:30:43 +0100

    improved a bit

--- src/httpGet/httpGet.go
+++ src/httpGet/httpGet.go
@@ -1,38 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+    "net/http"
+    "io/ioutil"
+    "encoding/xml"
+    "strings"
+    "log"
+    "fmt"
 )
 
-type valutes struct {
-	NumCode  string  `xml:"NumCode"`
-	CharCode string  `xml:"CharCode"`
-	Nominal  int     `xml:"Nominal"`
-	Name     string  `xml:"Name"`
-	Value    float64 `xml:"Value"`
+type Valutes struct {
+    XMLName  xml.Name `xml:"ValCurs"`
+    Valutes  []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+    XMLName  xml.Name `xml:"Valute"`
+    NumCode  string   `xml:"NumCode"`
+    CharCode string   `xml:"CharCode"`
+    Nominal  int      `xml:"Nominal"`
+    Name     string   `xml:"Name"`
+    Value    float64  `xml:"Value"`
 }
 
 func main() {
-	response, err := http.Get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=20/04/2019")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
+    response, err := http.Get("http://www.cbr.ru/scripts/XML_daily.asp?date_req=20/12/2019")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
 
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+    byteValue, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	//valute := &valutes{}
-	/*
-		err = xml.Unmarshal([]byte(contents), &valute)
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	fmt.Print(string(contents))
+    data := string(byteValue);
+    data = strings.Replace(data, "windows-1251", "UTF-8", 1)
+
+    //var valutes Valutes
+    //err = xml.Unmarshal([]byte(data), &valutes)
+    //if err != nil {
+    //    log.Fatal(err)
+    //}
+
+    fmt.Println(data)
+
+    //for i := 0; i < len(valutes.Valutes); i++ {
+    //    fmt.Println("CharCode:", valutes.Valutes[i].CharCode)
+    //    fmt.Println("Value:", valutes.Valutes[i].Value)
+    //}
 }
