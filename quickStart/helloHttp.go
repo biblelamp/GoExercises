2715dfaf4983bf1c75c1e76ea0fcbb1a23d07be9
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-02 17:36:11 +0100

    hello world http get header

--- quickStart/helloHttp.go
+++ quickStart/helloHttp.go
@@ -4,10 +4,15 @@ import (
     "fmt"
     "net/http"
 )
- 
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World!")
+    http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+        //fmt.Fprintf(w, "Hello World!")
+        for name, headers := range req.Header {
+            for _, h := range headers {
+                fmt.Fprintf(w, "%v: %v\n", name, h)
+            }
+        }
     })
 
     http.ListenAndServe(":80", nil)
