e7b6f9cb39319eee6bb00a8fb2808d9954c01ed7
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-22 11:43:15 +0100

    kettle example

---
+++ quickStart/kettle.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+    water := 1
+    println("Take a kettle")
+    if water == 0 {
+        println("Pour water into the kettle")
+    } else {
+        println("Enough water")
+    }
+    println("Put the kettle on fire")
+}
