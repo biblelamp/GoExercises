ef94c5e37b84593f09d25b476735073ae85d7817
Sergey Iryupin <biblelamp@yandex.ru>
2020-03-13 11:31:03 +0100

    improved a bit

--- quickStart/guessnumber.go
+++ quickStart/guessnumber.go
@@ -5,10 +5,11 @@ import "math/rand"
 import "time"
 
 func main() {
-    count := 0
-    guess := -1
     rand.Seed(time.Now().UnixNano())
-    number := rand.Intn(10)
+    count, guess, number := 0, -1, rand.Intn(10)
+    //count := 0
+    //guess := -1
+    //number := rand.Intn(10)
     for count < 3 && guess != number {
         fmt.Print("Guess [", 3 - count, " attempts] the number (0..9): ")
         fmt.Scanln(&guess)
