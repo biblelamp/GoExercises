3ed124aa385068da98ef558a3d638ab4471f678b
Sergey Iryupin <biblelamp@yandex.ru>
2020-05-12 12:56:27 +0200

    first implementation TYPE command

--- focal/src/interpreter/interpreter.go
+++ focal/src/interpreter/interpreter.go
@@ -1,18 +1,24 @@
 package interpreter
 
 import (
+	"bufio"
+	"calculations/calculate"
 	"fmt"
 	"messages"
+	"os"
 	"strings"
+	"util"
 )
 
 func Run() {
 	var line string
 	println(messages.WELCOME)
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		print(messages.PROMPT)
-		n, _ := fmt.Scanln(&line)
-		if n > 0 {
+		scanner.Scan()
+		line = scanner.Text()
+		if len(line) > 0 {
 			if result := processLine(line); result < 0 {
 				break
 			}
@@ -20,13 +26,45 @@ func Run() {
 	}
 }
 
+func commandAsk() {
+
+}
+
+func commandSet() {
+
+}
+
+func commandType(line string) {
+	parameters := util.SplitString(line[strings.Index(line, " ")+1:])
+	for _, item := range parameters {
+		if item[0:1] == "\"" {
+			fmt.Print(item[1 : len(item)-1])
+		} else if item[0:1] == "%" {
+			// TODO set format
+		} else if item[0:1] == "$" {
+			// TODO print all vbariables
+		} else if item == "!" {
+			fmt.Print("\n")
+		} else if item == "#" {
+			fmt.Print("\r")
+		} else if item == ":" {
+			fmt.Print("\t")
+		} else {
+			result := calculate.Calculate(item)
+			fmt.Print(result)
+		}
+	}
+}
+
 func processLine(line string) float32 {
-	parts := strings.Split(line, " ")
-	switch strings.ToUpper(parts[0]) {
+	tokens := strings.Split(line, " ")
+	switch strings.ToUpper(tokens[0]) {
+	case messages.T, messages.TYPE:
+		commandType(line)
 	case messages.Q, messages.QUIT:
 		return -1
 	default:
-		fmt.Printf(messages.COMMAND_NOT_RECOGNIZED, parts[0])
+		fmt.Printf(messages.COMMAND_NOT_RECOGNIZED, tokens[0])
 	}
 	return 0
 }
--- focal/src/messages/messages.go
+++ focal/src/messages/messages.go
@@ -1,8 +1,14 @@
 package messages
 
-const WELCOME = "GoFocal, version 0.1, 10 May 2020"
+const WELCOME = "GoFocal, version 0.1, 12 May 2020"
 const PROMPT = "*"
 
+const A = "A"
+const ASK = "ASK"
+const S = "S"
+const SET = "SET"
+const T = "T"
+const TYPE = "TYPE"
 const Q = "Q"
 const QUIT = "QUIT"
 
