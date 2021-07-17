f24849568280af39738f79dc66a27ec396994926
Sergey Iryupin <biblelamp@yandex.ru>
2021-07-17 08:39:35 +0200

    for + if + switch

---
+++ bookCalebDoxsey/chapter5/homework5.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i ++ {
+		fmt.Println(i)
+	}
+
+	for i := 0; i < 5; i ++ {
+		if i % 2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
+	}
+
+	for i := 0; i < 5; i ++ {
+		switch i {
+		case 0: fmt.Println("Zero")
+		case 1: fmt.Println("One")
+		case 2: fmt.Println("Two")
+		case 3: fmt.Println("Three")
+		case 4: fmt.Println("Four")
+		default: fmt.Println("Unknown Number")
+		}
+	}
+}
