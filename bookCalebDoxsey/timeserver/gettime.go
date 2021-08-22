ecfbe722a6d3e6fff7bf0f1951843576b4d2b516
Sergey Iryupin <biblelamp@yandex.ru>
2021-08-22 16:44:40 +0200

    time server

---
+++ bookCalebDoxsey/timeserver/gettime.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+}
