158f368b1b3ebfca5894f92538c3a057a2dea1e9
Sergey Iryupin <biblelamp@yandex.ru>
2021-08-23 16:46:32 +0200

    ping & pong

--- bookCalebDoxsey/chapter10/homework10.go
+++ bookCalebDoxsey/chapter10/homework10.go
@@ -6,9 +6,17 @@ import (
 )
 
 func main() {
-	go spinner(25 * time.Millisecond)
-	x := 45
-	fmt.Printf("Fibonacci(%d) = %d", x, fibonacci(x))
+	//go spinner(25 * time.Millisecond)
+	//x := 45
+	//fmt.Printf("Fibonacci(%d) = %d", x, fibonacci(x))
+
+	c := make(chan string)
+
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	time.Sleep(time.Second * 6)
 }
 
 func spinner(duration time.Duration) {
@@ -25,4 +33,24 @@ func fibonacci(x int) int {
 		return x
 	}
 	return fibonacci(x - 1) + fibonacci(x - 2)
+}
+
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <- c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
 }
