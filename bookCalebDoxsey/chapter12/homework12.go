9280524d9e9580123ef8fa312cbce58fc5ca9587
Sergey Iryupin <biblelamp@yandex.ru>
2021-09-02 15:24:46 +0200

    testing

---
+++ bookCalebDoxsey/chapter12/homework12.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"golang-book/math"
+)
+
+func main() {
+	fmt.Println(math.Average([]float64{1,2}))
+}
---
+++ bookCalebDoxsey/chapter12/math/math.go
@@ -0,0 +1,11 @@
+package math
+
+// real location C:\Program files\Go\src\...
+
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
