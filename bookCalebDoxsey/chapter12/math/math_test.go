948ce90c1ff8d08801807a5f501eeda15eac80a5
Sergey Iryupin <biblelamp@yandex.ru>
2021-09-04 12:01:28 +0200

    added test

--- bookCalebDoxsey/chapter12/math/math_test.go
+++ bookCalebDoxsey/chapter12/math/math_test.go
@@ -2,10 +2,34 @@ package math
 
 import "testing"
 
-func TestAverage(t *testing.T) {
+type testSet struct {
+	values []float64
+	average float64
+}
+
+var testSets = []testSet {
+	{ []float64{1,2}, 1.5 },
+	{ []float64{1,1,1,1,1,1}, 1 },
+	{ []float64{-1,1}, 0 },
+}
+
+func TestAverageOne(t *testing.T) {
 	var v float64
 	v = Average([]float64{1,2})
 	if v != 1.5 {
 		t.Error("Expected 1.5, got ", v)
+	}
+}
+
+func TestAverage(t *testing.T) {
+	for _, pair := range testSets {
+		v := Average(pair.values)
+		if v != pair.average {
+			t.Error(
+				"For", pair.values,
+				"expected", pair.average,
+				"got", v,
+			)
+		}
 	}
 }
